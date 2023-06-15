0064eda83108552f1c15dcb579697f07344953cc
Marc Horlacher <marc.horlacher@gmail.com>
2023-06-15 18:17:53 +0200

    added embed_from_multilabled_bed_gen

--- bend/io/sequtils.py
+++ bend/io/sequtils.py
@@ -1,4 +1,7 @@
+import tqdm
+import tensorflow as tf
 import pysam
+from bioio.tf.utils import multi_hot
 
 # %%
 baseComplement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
@@ -27,3 +30,22 @@ class Fasta():
             raise ValueError(f'Unknown strand: {strand}')
         
         return sequence
+
+# %%
+def embed_from_multilabled_bed_gen(bed, reference_fasta, embedder, label_column_idx, label_depth):
+    fasta = Fasta(reference_fasta)
+    with open(bed) as f:
+        for line in tqdm.tqdm(f):
+            # get bed row
+            row = line.strip().split('\t')
+            chrom, start, end, strand = row[0], int(row[1]), int(row[2]), row[5]
+            labels = list(map(int, row[label_column_idx].split(',')))
+
+            # get sequence
+            sequence = fasta.fetch(chrom, start, end, strand)
+
+            # embed sequence and multi-hot encode labels
+            sequence_emebd = tf.squeeze(tf.constant(embedder(sequence)))
+            labels_multi_hot = multi_hot(labels, depth=label_depth)
+
+            yield {'inputs': sequence_emebd, 'outputs': labels_multi_hot}
