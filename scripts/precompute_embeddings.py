18b558af38a7e0facdb8cc3f6ed3f8fdffdeb120
Frederikke Marin <frma@koala.sdma.nzcorp.net>
2023-10-03 22:12:24 +0200

    minor fixes

--- scripts/precompute_embeddings.py
+++ scripts/precompute_embeddings.py
@@ -36,14 +36,15 @@ def run_experiment(cfg: DictConfig) -> None:
         # get length of bed file and divide by chunk size, if a spcific chunk is not set 
         df = pd.read_csv(cfg[cfg.task].bed, sep = '\t', low_memory=False)
         df = df[df.iloc[:, -1] == split] if split is not None else df
-        possible_chunks = list(range(int(len(df) /cfg.chunk_size)+1))         
+        possible_chunks = list(range(int(len(df) /cfg.chunk_size)+1))
         if cfg.chunk is None: 
             cfg.chunk = possible_chunks
+        cfg.chunk = [cfg.chunk] if isinstance(cfg.chunk, int) else cfg.chunk
         # embed in chunks
         for n, chunk in enumerate(cfg.chunk): 
+            if chunk not in possible_chunks:
+                return f'{chunk} is not a valid chunk id. {split} chunk ids are {possible_chunks}'
             print(f'Embedding chunk {chunk}/{len(possible_chunks)}')
-
-            
             sequtils.embed_from_bed(**cfg[cfg.task], embedder = embedder, 
                                         output_path = f'{output_dir}/{split}_{chunk}.tar.gz',
                                         split = split, chunk = chunk, chunk_size = cfg.chunk_size,   
