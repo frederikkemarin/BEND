c2bdff9fbfd429c6babcc7990a0fd600c87af2e6
Frederikke Marin <frma@koala.sdma.nzcorp.net>
2023-10-10 15:58:35 +0200

    print split

--- scripts/precompute_embeddings.py
+++ scripts/precompute_embeddings.py
@@ -27,7 +27,7 @@ def run_experiment(cfg: DictConfig) -> None:
     # instatiante model
     embedder = hydra.utils.instantiate(cfg[cfg.model])
     for split in splits:
-        print(f'Embedding {split} set')
+        print(f'Embedding split: {split}')
         output_dir = f'{cfg.data_dir}/{cfg.task}/{cfg.model}/'
         
         os.makedirs(output_dir, exist_ok=True)
@@ -41,12 +41,11 @@ def run_experiment(cfg: DictConfig) -> None:
             cfg.chunk = possible_chunks
         cfg.chunk = [cfg.chunk] if isinstance(cfg.chunk, int) else cfg.chunk
         # embed in chunks
-        print(possible_chunks)
         for n, chunk in enumerate(cfg.chunk): 
             if chunk not in possible_chunks:
                 print(f'{chunk} is not a valid chunk id. {split} chunk ids are {possible_chunks}')
                 continue
-            print(f'Embedding chunk {chunk} ({chunk +1}/{len(possible_chunks)})')
+            print(f'\t Embedding chunk {chunk} ({chunk +1}/{len(possible_chunks)})')
             sequtils.embed_from_bed(**cfg[cfg.task], embedder = embedder, 
                                         output_path = f'{output_dir}/{split}_{chunk}.tar.gz',
                                         split = split, chunk = chunk, chunk_size = cfg.chunk_size,   
